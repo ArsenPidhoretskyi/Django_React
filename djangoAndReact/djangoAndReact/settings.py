bb037ca7cd8b6ad5c870ceccc9ddab89df70150b
Arsen <arsen.pidhoretskyi@gmail.com>
2021-01-28 20:36:12 +0200

    Rebuild project + migrations

--- djangoAndReact/djangoAndReact/settings.py
+++ djangoAndReact/djangoAndReact/settings.py
@@ -76,16 +76,14 @@ WSGI_APPLICATION = 'djangoAndReact.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-    'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    'NAME': 'django_db',
-    'USER': 'user_name',
-    'PASSWORD': 'password',
-    'HOST': '127.0.0.1',
-    'PORT': '5432',
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'todoservice',
+        'USER': 'postgres',
+        'PASSWORD': 'arsen_postgres',
+        'HOST': '127.0.0.1',
+        'PORT': '2000',
+    }
 }
 
 
